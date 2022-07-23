5c6d0fd4b86eb78d08bc9078d5e77620cc934cf4
vijith6600 <vijith4y@gmail.com>
2022-07-23 10:19:09 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,14 @@
+// 1.display numbers in text box
+
+function displaynum(num) {
+    display.value+=num
+    
+}
+
+function acbutton() {
+    display.value=""
+}
+
+function evaluateNum() {
+    display.value = eval(display.value)
+}
